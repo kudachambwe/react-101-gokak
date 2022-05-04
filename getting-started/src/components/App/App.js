a049036f35569e125faadef4ddf56c92f006668c
Kuda Chambwe <kkchambwe@gmail.com>
2022-05-05 00:18:07 +0200

    cleanup duplicate git tracking

---
+++ getting-started/src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "../../features/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="">Hello World</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
